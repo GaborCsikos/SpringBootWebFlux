ba93914600c2a8100835d321018a9957d37e9fd1
gcsikos <csiki123@gmail.com>
2019-12-23 18:12:41 +0100

    added filters

--- src/test/java/gabor/csikos/reactive/web/webflux/FluxAndMonoTest.java
+++ src/test/java/gabor/csikos/reactive/web/webflux/FluxAndMonoTest.java
@@ -1,9 +1,14 @@
 package gabor.csikos.reactive.web.webflux;
 
+import org.apache.commons.compress.utils.Lists;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class FluxAndMonoTest {
 
@@ -17,6 +22,19 @@ public class FluxAndMonoTest {
     public void fluxTest(){
         Flux<String> flux = Flux.just("First", "Second", "Third");
         StepVerifier.create(flux).expectNextCount(3).verifyComplete();
+    }
+
+    @Test
+    public void fluxTestRange(){
+        Flux<Integer> flux = Flux.range(1,3);
+        StepVerifier.create(flux).expectNext(1,2,3).verifyComplete();
+    }
+
+    @Test
+    public void fluxTestFilter(){
+        List<String> names = Arrays.asList("Gabor", "Szabina");
+        Flux<String> flux = Flux.fromIterable(names).filter(name -> name.startsWith("S"));
+        StepVerifier.create(flux).expectNext("Szabina").verifyComplete();
     }
 
     @Test
